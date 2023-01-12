9bfad2d5c4e7b001d91963c152f8b8b57168ba3c
Andrea Piano <andreapiano@Andreas-MacBook-Pro.local>
2023-01-12 21:51:58 +0100

    feat: fixed some typos

--- pages/components/container/Contact.jsx
+++ pages/components/container/Contact.jsx
@@ -55,7 +55,7 @@ const Contact = () => {
       preheader: "Thank you for contacting me",
       greetings: `Dear ${firstName} ${lastName},`,
       message:
-        "Thank you for contacting me! I will review your request and will be in touch within few days. </br> In the meantime, make sure you follow me on <b>LinkedIn</b> (link below), to stay updated with my personal life, creations and career achievements. </br> </br> Met vriendelijke groet, </br> Andrea Piano",
+        "Thank you for contacting me! I will review your request and will be in touch within a few days. </br> In the meantime, make sure you follow me on <b>LinkedIn</b> (link below), to stay updated with my personal life, creations and career achievements. </br> </br> Met vriendelijke groet, </br> Andrea Piano",
       callToAction: {
         name: "Follow me on LinkedIn",
         href: "https://www.linkedin.com/in/andreapiano/",
@@ -178,8 +178,8 @@ const Contact = () => {
           {emailSent && (
             <div className="email-sent">
               <h1>
-                Thank you for your message! <br /> You'll receive an email as a
-                confirmation soon.
+                Thank you for your message! <br /> You'll receive a confirmation
+                email soon.
               </h1>
               <div className="success-container">
                 <Image
