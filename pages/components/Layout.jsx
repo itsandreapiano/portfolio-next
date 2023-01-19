d06b9288e494836c1355b776c4ac1d91a8d5f1be
ateyapayo <andrea.piano@softfobia.com>
2023-01-19 12:02:18 +0100

    feat: improved font

--- pages/components/Layout.jsx
+++ pages/components/Layout.jsx
@@ -16,7 +16,7 @@ const Layout = ({ children }) => {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;650&display=swap" rel="stylesheet"></link>
       </Head>
       <div className="body">
         <Nav />
